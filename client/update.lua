94f0d01aa9e916b873f46b7a23ca03c863c93ac5
olliejohnson <oliver.joseph.johnson@gmail.com>
2024-04-10 19:39:17 -0400

    Added Update Script

---
+++ client/update.lua
@@ -0,0 +1,24 @@
+urls = {
+    {"client", "https://raw.githubusercontent.com/olliejohnson/networked-payments/main/client/client.lua"},
+    {"cryptoNet", "https://raw.githubusercontent.com/olliejohnson/networked-payments/main/client/cryptoNet.lua"}
+}
+
+function download(name, url)
+    request = http.get(url)
+    data = request.readAll()
+
+    if fs.exists(name) then
+        fs.delete(name)
+        file = fs.open(name, "w")
+        file.write(data)
+        file.close()
+    else
+        file = fs.open(name, "w")
+        file.write(data)
+        file.close()
+    end
+end
+
+for key, value in ipairs(urls) do
+    download(unpack(value))
+end
